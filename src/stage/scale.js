50ca3c8d3649cc64d775f755dee2b89cb135e782
xleine <xleine@qq.com>
2024-12-21 13:42:07 +0800

    feat: adjust scale logic

--- src/stage/scale.js
+++ src/stage/scale.js
@@ -1,10 +1,10 @@
-export function drawTextWithScale(canvas, tip) {
+export function drawTextWithScale(canvas, canvasType) {
   const crossLineHalfWidth = 25;
-  const step = 60;
+  const step = 70;
   const x = 90;
   let y = 60;
   let fontFamily = "zcool-gdh";
-  let text = "The quick brown fox jumps over the lazy dog";
+  let text = "拿一行字来测试";
 
   let ctx = canvas.getContext("2d");
 
@@ -22,41 +22,77 @@ export function drawTextWithScale(canvas, tip) {
     {
       scaleX: 1,
       scaleY: 1,
-      letterSpacing: "-1px",
+      letterSpacing: "-10px",
+    },
+    {
+      scaleX: 1,
+      scaleY: 1,
+      letterSpacing: "10px",
     },
     {
       scaleX: 0.5,
       scaleY: 1,
-      letterSpacing: "1px",
-    },
-    {
-      scaleX: 1,
-      scaleY: 0.5,
-      letterSpacing: "10px",
+      letterSpacing: "20px",
     },
     {
       scaleX: 0.8,
       scaleY: 1,
-      letterSpacing: "12.5px",
+      letterSpacing: "50px",
     },
     {
-      scaleX: 1,
+      scaleX: 0.8,
       scaleY: 1,
       letterSpacing: "-10px",
     },
+    {
+      scaleX: 0.8,
+      scaleY: 1,
+      letterSpacing: "-50px",
+    },
+    {
+      scaleX: 1,
+      scaleY: 0.5,
+      letterSpacing: "20px",
+    },
+    {
+      scaleX: 1,
+      scaleY: 0.5,
+      letterSpacing: "-20px",
+    },
+    {
+      scaleX: 1,
+      scaleY: 0.8,
+      letterSpacing: "50px",
+    },
+    {
+      scaleX: 0.8,
+      scaleY: 0.8,
+      letterSpacing: "50px",
+    },
+    {
+      scaleX: 0.8,
+      scaleY: 0.8,
+      letterSpacing: "-50px",
+    },
   ];
 
+
   // draw tip
-  drawTextWithTip(tip);
+  const tip = `text tip is (scaleX, scaleY, letterSpacing)`
+  drawTextWithTip(canvasType, x, y);
+  drawTextWithTip3(tip, x + crossLineHalfWidth, y + crossLineHalfWidth);
 
   // with letterSpacing
   baselines.forEach(({ scaleX, scaleY, letterSpacing }, index) => {
     const newY = y + step * (index + 1);
     drawCrossLine(x, newY);
+    drawTextWithTip2(`${scaleX}, ${scaleY}, ${letterSpacing}`, x - 10, newY);
+
     drawTextWithScale(text, x, newY, scaleX, scaleY, letterSpacing);
   });
 
   function drawTextWithScale(text, x, y, scaleX, scaleY, letterSpacing) {
+    ctx.save()
     ctx.setTransform(scaleX, 0, 0, scaleY, 0, 0);
     ctx.letterSpacing = letterSpacing;
 
@@ -68,6 +104,7 @@ export function drawTextWithScale(canvas, tip) {
     ctx.fillText(text, newX, newY);
 
     ctx.resetTransform();
+    ctx.restore();
   }
 
   function drawCrossLine(x, y) {
@@ -80,8 +117,28 @@ export function drawTextWithScale(canvas, tip) {
     ctx.stroke();
   }
 
-  function drawTextWithTip(text) {
+  function drawTextWithTip(text, x, y) {
     ctx.textBaseline = "alphabetic";
     ctx.fillText(text, x, y);
+  }
+
+  function drawTextWithTip2(text, x, y) {
+    ctx.save()
+
+    ctx.font = `normal 10pt 'Times new roman'`;
+    ctx.textBaseline = "alphabetic";
+    ctx.fillText(text, x + crossLineHalfWidth, y - 10);
+    
+    ctx.restore();
+  }
+
+  function drawTextWithTip3(text, x, y) {
+    ctx.save()
+
+    ctx.font = `normal 20pt 'Times new roman'`;
+    ctx.textBaseline = "alphabetic";
+    ctx.fillText(text, x, y);
+    
+    ctx.restore();
   }
 }
